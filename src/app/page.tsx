dc8da8cdd0f8559502527d29e4515226704f83db
genuka manthila <genukamanthila21@gmail.com>
2024-10-15 08:52:06 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { HomePage } from "@/pages";
+
+export default function Home() {
+  return (
+      <main className="overflow-hidden">
+        <HomePage/>
+
+      </main>
+     
+     
+
+  
+  );
+}
