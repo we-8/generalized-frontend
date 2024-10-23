39def9e1feadbec37602fb360a62bfbb14b1649d
Genuka-we8 <genuka.manthila@we8.io>
2024-10-23 11:31:50 +0530

    hero banner

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,11 @@
 import testImage from './images/test.jpg';
 import logo from './images/logo.png';
 import cart from './images/grocery-store.png';
+import hero from './images/hero.jpg';
 
 export{
   testImage,
   logo,
-  cart
+  cart,
+  hero
 }
---
+++ src/components/Herobanner/Herobanner.tsx
@@ -0,0 +1,11 @@
+import '../../styles/components-css/Herobanner.css'
+
+const Herobanner = () => {
+  return (
+    <div className='app__herobanner--main-div'>
+      grhfghjfgh fhero fg
+    </div>
+  )
+}
+
+export default Herobanner;
