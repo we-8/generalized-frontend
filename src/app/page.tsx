231db7f8dfacbc85b0fd04f2f4331a33bba2836a
Genuka-we8 <genuka.manthila@we8.io>
2024-10-24 11:49:07 +0530

    resoponsive category section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import { Herobanner } from "@/components";
-import { hero2 } from "@/assets";
+import { hero2,test1 } from "@/assets";
 import '../styles/main-pages-css/HomePage.css';
 
-const categories =['Test1','Test2','Test3','Test4']
+type Category = {
+  title: string;
+};
+const categories:Category[] =[{title:'Test1'},{title:'Test2'},{title:'Test3'},{title:'Test4'}]
 
 
 
@@ -12,6 +15,12 @@ export default function Home() {
       <div className="app__home--main-div">
         <Herobanner backgroundImage={hero2} title="Leading vacuum food & product ingredient supplier" description="Body text for your whole article or post. We’ll put in some lorem ipsum to show how a filled-out page might look" />
         <div className="app__home--category-section section__padding">
+          {categories.map((categories,index)=>(
+            <div key={index} className="app__home--category-item">
+              <Image className="category-image" src={test1} alt="category image"/>
+              <p className="category-title">{categories.title}</p>
+            </div>
+          ))}
 
         </div>
         
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,9 +3,11 @@ import logo from './images/logo.png';
 import cart from './images/grocery-store.png';
 import hero from './images/hero.jpg';
 import hero2 from './images/hero2.jpg';
+import test1 from './images/testimg.jpg'
 
 export{
   testImage,
+  test1,
   logo,
   cart,
   hero,
