189c04bee0073c85816776e80afeeea522c42fa7
Genuka-we8 <genuka.manthila@we8.io>
2024-11-18 00:22:34 +0530

    buttons updated

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@ import { Herobanner, Herobanner2 } from './Herobanner/Herobanner';
 import {Title,TitleL,TitleR ,TitleYellow} from './Title/Title';
 import {DescriptionL,DescriptionR,Description} from './Description/Description';
 import SpecialOffers from './SpecialOffer/SpecialOffer';
-import {AddtoCartSingleProdocut,ProductFilter,GoogleButton,Login,BackToShop, CommonButtons ,CommonButtons1 ,OrderNow,OrderNow2,AddtoCart , RemoveButton , CheckOut} from './CommonButtons/CommonButtons';
+import {ProfileSaveChange,AddtoCartSingleProdocut,ProductFilter,GoogleButton,Login,BackToShop, CommonButtons ,CommonButtons1 ,OrderNow,OrderNow2,AddtoCart , RemoveButton , CheckOut} from './CommonButtons/CommonButtons';
 import LatestProducts from './LatestProducts/LatestProducts';
 import PeopleReview from './PeopleReview/PeopleReview';
 import SearchBar from './SearchBar/SearchBar';
@@ -33,6 +33,7 @@ export{
   CommonButtons,
   CommonButtons1,
   ProductFilter,
+  ProfileSaveChange,
   Title,
   TitleL,
   TitleR,
