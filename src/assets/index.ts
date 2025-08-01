888d9c437f6b8eb8c8e381b722d867dc4b94e303
Kavindu Jayasinghe <kavindu.20210951@iit.ac.lk>
2025-08-01 09:42:31 +0530

    exporting of banner images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,28 +1,33 @@
-import testImage from './images/test.jpg';
-import logo from './images/logo.png';
-import cart from './images/grocery-store.png';
-import hero from './images/hero.jpg';
-import hero2 from './images/hero2.jpg';
-import test1 from './images/testimg.jpg'
-import who from './images/who.jpg';
-import test2 from './images/specialtest.png';
-import test3 from './images/test2.jpg';
-import person from './images/person.jpg';
-import hero3 from './images/hero3.jpg';
-import hero4 from './images/hero4.jpg';
-import Cashew from './images/cashew.png';
-import Facebook from './images/facebook.png';
-import Tiktok from './images/tiktok.png';
-import Instagram from './images/instagram.png';
-import Youtube from './images/youtube.png';
-import Google from './images/google.png';
-import Quote from './images/double-quotes.png';
-import animation1 from './animations/lottie.json';
-import angryani from './animations/angry.json';
-import sadani from './animations/sad.json';
-import neutralani from './animations/neutral.json';
-import smileani from './animations/smile.json';
-export{
+import testImage from "./images/test.jpg";
+import logo from "./images/logo.png";
+import cart from "./images/grocery-store.png";
+import hero from "./images/hero.jpg";
+import hero2 from "./images/hero2.jpg";
+import test1 from "./images/testimg.jpg";
+import who from "./images/who.jpg";
+import test2 from "./images/specialtest.png";
+import test3 from "./images/test2.jpg";
+import person from "./images/person.jpg";
+import hero3 from "./images/hero3.jpg";
+import hero4 from "./images/hero4.jpg";
+import Cashew from "./images/cashew.png";
+import Facebook from "./images/facebook.png";
+import Tiktok from "./images/tiktok.png";
+import Instagram from "./images/instagram.png";
+import Youtube from "./images/youtube.png";
+import Google from "./images/google.png";
+import Quote from "./images/double-quotes.png";
+import animation1 from "./animations/lottie.json";
+import angryani from "./animations/angry.json";
+import sadani from "./animations/sad.json";
+import neutralani from "./animations/neutral.json";
+import smileani from "./animations/smile.json";
+import home_banner from "./images/home_banner.jpg";
+import About_us_banner from "./images/About_us_banner.jpg";
+import Contact_us_banner from "./images/Contact_us_banner.jpg";
+import Products_banner from "./images/Products_banner.jpg";
+
+export {
   testImage,
   animation1,
   smileani,
@@ -46,5 +51,9 @@ export{
   hero,
   hero2,
   hero3,
-  hero4
-}
+  hero4,
+  home_banner,
+  About_us_banner,
+  Contact_us_banner,
+  Products_banner,
+};
