17daafb7c8e5ae97ac0b0c333beea6981dc4830f
Kavindu Jayasinghe <kavindu.20210951@iit.ac.lk>
2025-08-01 10:37:37 +0530

    updating Products page banner image

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,5 +1,5 @@
 import { Herobanner2 ,ProductSection} from "@/components";
-import { hero3 } from "@/assets";
+import { Products_banner } from "@/assets";
 import React,{useState} from "react";
 import '../../styles/main-pages-css/ProductPage.css';
 
@@ -8,7 +8,7 @@ const Product = () => {
   return (
     <div className="app__product--main-div">
       <div>
-      <Herobanner2 backgroundImage={hero3} title="Our Products" description="Body text for your whole article or post. We’ll put in some lorem ipsum to show how a filled-out page might look:" />
+      <Herobanner2 backgroundImage={Products_banner} title="Our Products" description="Body text for your whole article or post. We’ll put in some lorem ipsum to show how a filled-out page might look:" />
       </div>
       <div>
         <ProductSection/>
